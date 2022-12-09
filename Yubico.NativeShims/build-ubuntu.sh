9bc0ff422583356b91ef709cb3b73542695b0c5f
kindtek <n8@kindtek.io>
2022-12-09 05:17:47 -0800

    string cleanup

--- Yubico.NativeShims/build-ubuntu.sh
+++ Yubico.NativeShims/build-ubuntu.sh
@@ -57,7 +57,7 @@ docker run --pull always --rm --privileged multiarch/qemu-user-static --reset -p
 # Arch: amd64/x64
 # Output: ./ubuntu-x64/libYubico.NativeShims.so
 docker buildx build \
-    --tag ${SDB_YUB_DOCKER_IMG:-kindtek/yubico-safedb-ubuntu} \
+    --tag ${SDB_YUB_DOCKER_IMG:-'kindtek/yubico-safedb-ubuntu'} \
     --file docker/Ubuntu/Dockerfile \
     --platform=linux/amd64 \
     --build-arg USER_ID=${YUBICO_USER_ID:-1000} \
@@ -70,7 +70,7 @@ docker buildx build \
 # Arch: i386/x86
 # Output: ./ubuntu-x86/libYubico.NativeShims.so
 docker buildx build \
-    --tag ${SDB_YUB_DOCKER_IMG:-kindtek/yubico-safedb-ubuntu} \
+    --tag ${SDB_YUB_DOCKER_IMG:-'kindtek/yubico-safedb-ubuntu'} \
     --file docker/Ubuntu/Dockerfile \
     --platform=linux/386 \
     --build-arg USER_ID=${YUBICO_USER_ID:-1000} \
@@ -83,7 +83,7 @@ docker buildx build \
 # Arch: arm64
 # Output: ./ubuntu-arm64/libYubico.NativeShims.so
 docker buildx build \
-    --tag ${SDB_YUB_DOCKER_IMG:-kindtek/yubico-safedb-ubuntu} \
+    --tag ${SDB_YUB_DOCKER_IMG:-'kindtek/yubico-safedb-ubuntu'} \
     --file docker/Ubuntu/Dockerfile \
     --platform=linux/arm64 \
     --build-arg USER_ID=${YUBICO_USER_ID:-1000} \
